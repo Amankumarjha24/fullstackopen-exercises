a034ed3c7cd594066b8f03bb0e2e96716cdc78aa
Aman kumar <ajha00821@gmail.com>
2026-08-12 15:32:41 +0530

    Delete Part-2 directory

--- Part-2/src/component/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({search, getSearch}) => {
-    return (
-        <div>
-            Filter shown with: <input type="text"  value={search} onChange={getSearch} />
-        </div>
-    )
-}
-
-export default Filter
